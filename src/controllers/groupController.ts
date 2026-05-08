8e6c5f9df6032b897ec92e1c907352801099bd86
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2026-05-08 19:45:26 -0300

    [FIX] sorting groups in alphabetical order

--- src/controllers/groupController.ts
+++ src/controllers/groupController.ts
@@ -6,33 +6,60 @@ const groupController = {
     // Todos os grupos
     async getGroups(req: Request, res: Response) {
         try {
-            const groups = await prisma.group.findMany();
+
+            const groups =
+                await prisma.group.findMany({
+                    orderBy: {
+                        name: "asc"
+                    }
+                });
 
             return res.json(groups);
+
         } catch (error) {
-            console.error("Erro ao buscar grupos:", error);
+
+            console.error(
+                "Erro ao buscar grupos:",
+                error
+            );
 
             return res.status(500).json({
-                error: "Erro interno ao buscar grupos"
+                error:
+                    "Erro interno ao buscar grupos"
             });
         }
     },
 
     // Grupos em destaque
-    async getFeaturedGroups(req: Request, res: Response) {
+    async getFeaturedGroups(
+        req: Request,
+        res: Response
+    ) {
+
         try {
-            const groups = await prisma.group.findMany({
-                where: {
-                    isFeatured: true
-                }
-            });
+
+            const groups =
+                await prisma.group.findMany({
+                    where: {
+                        isFeatured: true
+                    },
+                    orderBy: {
+                        name: "asc"
+                    }
+                });
 
             return res.json(groups);
+
         } catch (error) {
-            console.error("Erro ao buscar grupos:", error);
+
+            console.error(
+                "Erro ao buscar grupos:",
+                error
+            );
 
             return res.status(500).json({
-                error: "Erro interno ao buscar grupos"
+                error:
+                    "Erro interno ao buscar grupos"
             });
         }
     }
